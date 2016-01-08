01015a75e16cd9be1f394bc64b34feca929fa7c3
Saddam <saddammonero@gmail.com>
2016-01-08 11:43:18 -0600

    add note for create method and itemOwnership

--- test.js
+++ test.js
@@ -1,13 +1,30 @@
-var CryptoKingdom = require('cryptokingdom'),
-    username = 'YOUR USERNAME',
-    password = 'YOUR PASSWORD',
-    deviceID = 'YOUR DEVICE ID', //an arbitrary number
+var CryptoKingdom = require('./lib/cryptokingdom'),
+    username = 'Riddick',
+    password = 'dO`j9n~TXr4P',
+    deviceID = '2', //an arbitrary number
     ck = new CryptoKingdom(username, password, deviceID);
 
 //example of get state for item
-ck.getStateForItem('W1601', function(err, response){
+//ck.create('MEAD', 1, 57, function(err, response){
+//    if(err){
+//        console.log(err);
+//    }
+//    console.log(response.data);
+//});
+
+ck.itemOwnership(function(err, data){
     if(err){
         console.log(err);
+    } else {
+        console.log(data);
     }
-    console.log(response.data);
 });
+
+//ck.consume('can', 1, 'test', function(err, res) {
+//    if(err){
+//        console.log(err);
+//    } else {
+//        console.log(res);
+//    }
+//});
+
