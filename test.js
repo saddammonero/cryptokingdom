9f3b8b6186de58d4f04f61846b8a2a6a8dce4240
Saddam <saddammonero@gmail.com>
2016-02-11 17:09:53 -0600

    added item ownership command

--- test.js
+++ test.js
@@ -1,14 +1,23 @@
 var CryptoKingdom = require('./lib/cryptokingdom'),
-    username = 'USERNAME',
+    username = 'username',
     password = 'PASSWORD',
-    deviceID = '2', //an arbitrary number
+    deviceID = 'test', //an arbitrary id
     ck = new CryptoKingdom(username, password, deviceID);
 
-ck.consume('can', 1, 'test', function(err, res) {
-    if(err){
-        console.log(err);
-    } else {
-        console.log(res);
-    }
+//ck.consume('can', 1, 'test', function(err, res) {
+//    if(err){
+//        console.log(err);
+//    } else {
+//        console.log(res);
+//    }
+//});
+
+//ck.getToken(ck, function(err, token){
+//    console.log(token);
+//});
+
+ck.itemOwnership(token, item, function(err, data){
+    console.log(data);
 });
+
 
